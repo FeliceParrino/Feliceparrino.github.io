2205608a3acabf3139f989bb12f6a2574cf070cb
Felice Parrino <124209112+FeliceParrino@users.noreply.github.com>
2024-04-11 15:48:15 +0100

    Delete Feliceparrino.github.io-main directory

--- Feliceparrino.github.io-main/Contacts.js
+++
@@ -1,9 +0,0 @@
-// JavaScript per la gestione del modulo di contatto
-const contactForm = document.getElementById('contactForm');
-
-contactForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Aggiungi qui la logica per inviare il modulo di contatto
-    alert('Modulo di contatto inviato con successo!');
-    this.reset(); // Resetta il modulo dopo l'invio
-});
