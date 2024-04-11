190debeb9531c56436afdb3c5dd8e060dd4e6355
Felice Parrino <124209112+FeliceParrino@users.noreply.github.com>
2024-04-11 15:44:26 +0100

    Add files via upload

---
+++ Feliceparrino.github.io-main/Script.js
@@ -0,0 +1,38 @@
+// Funzione per gestire lo scorrimento fluido quando si fa clic sui link della navigazione
+function smoothScroll(target, duration) {
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+// Gestisce lo scorrimento fluido quando si fa clic sui link della navigazione
+var navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        var target = this.getAttribute('href');
+        smoothScroll(target, 1000);
+    });
+});
+
+
